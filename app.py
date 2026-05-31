865b93bc459fc0a6d565854c6e5df18e4861b126
Harikrishnan-E-M <harikrishnan.e.m7@gmail.com>
2026-05-31 21:48:24 +0530

    rend

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=os.getenv("FLASK_DEBUG", "1") == "1", use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.getenv("FLASK_DEBUG", "1") == "1", use_reloader=False)
